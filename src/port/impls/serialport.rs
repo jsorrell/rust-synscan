0fe89e4163cec45c735649fde95aea4be77acdc1
Jack Sorrell <jack@jacksorrell.com>
2022-02-13 17:27:49 -0500

    Alias type for serialport's SerialPort

--- src/port/impls/serialport.rs
+++ src/port/impls/serialport.rs
@@ -3,17 +3,19 @@ use crate::{MotorController, SerialPort};
 use std::io;
 use std::time::Duration;
 
-#[cfg(feature = "serialport")]
-impl SerialPort for Box<dyn serialport::SerialPort> {}
+pub type SPSerialPort = Box<dyn serialport::SerialPort>;
 
 #[cfg(feature = "serialport")]
-impl MotorController<Box<dyn serialport::SerialPort>> {
+impl SerialPort for SPSerialPort {}
+
+#[cfg(feature = "serialport")]
+impl MotorController<SPSerialPort> {
     /// Gets a new MotorController using the serialport implentation of a serial port
     pub fn new_serialport(
         path: impl Into<String>,
         baud_rate: u32,
         timeout: Duration,
-    ) -> SynScanResult<MotorController<Box<dyn serialport::SerialPort>>> {
+    ) -> SynScanResult<MotorController<SPSerialPort>> {
         let port = serialport::new(path.into(), baud_rate)
             .timeout(timeout)
             .open()?;
